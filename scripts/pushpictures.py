0962525899f591dce2f622f65ced0a34a3c38776
Pinball Machine <vmvpin@gmail.com>
2024-03-12 03:49:00 +0100

    Updated high scores md files

--- scripts/pushpictures.py
+++ scripts/pushpictures.py
@@ -2,7 +2,7 @@ from git import Repo
 
 repo = Repo("C:\\Visual Pinball\\vmvpin.github.io")
 remote = repo.remote()
-repo.git.add("-A")
+repo.git.add()
 if repo.is_dirty():
     repo.index.commit("Updated high scores")
 remote.pull()
