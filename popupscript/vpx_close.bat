657e8c654d1cb5779a1ca5eb171a6152bd8da61b
Pinball Machine <vmvpin@gmail.com>
2024-05-23 11:06:08 +0200

    Hacked commit

--- popupscript/vpx_close.bat
+++ popupscript/vpx_close.bat
@@ -1,5 +1,5 @@
 REM Call this batch file from Popper's Visual Pinball X Close Script.  It should be the last line.
-REM CALL [STARTDIR]Scripts\vpx_launch.bat "[DIREMU]" "[DIRGAME]" "[DIRROM]" "[GAMEFULLNAME]" "[GAMENAME]" "[GAMEEXT]" "[STARTDIR]" "[CUSTOM1]" "[CUSTOM2]" "[CUSTOM3]" "[ALTEXE]" "[ALTMODE]" "[MEDIADIR]" "[?ROM?]" "[?GameType?]"
+REM CALL [STARTDIR]Hiscore\vpx_launch.bat "[DIREMU]" "[DIRGAME]" "[DIRROM]" "[GAMEFULLNAME]" "[GAMENAME]" "[GAMEEXT]" "[STARTDIR]" "[CUSTOM1]" "[CUSTOM2]" "[CUSTOM3]" "[ALTEXE]" "[ALTMODE]" "[MEDIADIR]" "[?ROM?]" "[?GameType?]" 
 
 REM Use shift since we have more than 9 parameters
 REM Remove quotes from variables
@@ -32,26 +32,50 @@ SHIFT
 REM ?game_field? entries...
 SET ROM=%~1
 SHIFT
-SET GameType=%~1
+SET GAMETYPE=%~1
 SHIFT
 
-set currentTIme=%time%
-
-ECHO %currentTIme% VPX close script start>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO VPX close script start for %GAMENAME% >> "c:\Visual~1\Hiscores\logs\debug.log"
 
 IF NOT "%ROM%"=="" (
-    ECHO Starting high score generation...>> "%STARTDIR%\scripts\logs\debug.log"
-    ECHO "%STARTDIR%Scripts\hiscore.bat" %ROM% "%GAMENAME%" "%GameType%">> "%STARTDIR%\scripts\logs\debug.log"
+   
+    ECHO Cutom var was %CUSTOM2% >> "c:\Visual~1\Hiscores\logs\debug.log"
+    IF "%CUSTOM2%"=="UltraDMD" (
+        ECHO Starting high score generation...>> "c:\Visual~1\Hiscores\logs\debug.log"
+        ECHO "c:\Visual~1\Hiscores\hiscore.bat" %ROM% "%GAMENAME%" "%CUSTOM2%">> "c:\Visual~1\Hiscores\logs\debug.log"
 
-    REM Generate HiScore media file
-    CALL "%STARTDIR%Scripts\hiscore.bat" %ROM% "%GAMENAME%" "%GameType%"
-    ECHO Completed high score generation>> "%STARTDIR%\scripts\logs\debug.log"
+        REM Generate HiScore media file
+        CALL "c:\Visual~1\Hiscores\hiscore.bat" %ROM% "%GAMENAME%" "%CUSTOM2%"
+        ECHO Completed high score generation>> "c:\Visual~1\Hiscores\logs\debug.log"
+        
+    ) ELSE (
+        ECHO Starting high score generation...>> "c:\Visual~1\Hiscores\logs\debug.log"
+        ECHO "c:\Visual~1\Hiscores\hiscore.bat" %ROM% "%GAMENAME%" "%GAMETYPE%">> "c:\Visual~1\Hiscores\logs\debug.log"
+
+        REM Generate HiScore media file
+        CALL "c:\Visual~1\Hiscores\hiscore.bat" %ROM% "%GAMENAME%" "%GAMETYPE%"
+        ECHO Completed high score generation>> "c:\Visual~1\Hiscores\logs\debug.log"
+    )
 )
 
-ECHO Restoring settings...>> "%STARTDIR%\scripts\logs\debug.log"
-CALL "%STARTDIR%restore_settings.bat"
-REM DEL "%STARTDIR%restore_settings.bat"
+ECHO generating md files and pushing to git >> "c:\Visual~1\Hiscores\logs\debug.log" 
+REM python "C:\\Visual Pinball\\vmvpin.github.io\\scripts\\genmd.py"
+CALL CMD /C python "C:\\Visual Pinball\\vmvpin.github.io\\scripts\\genmd.py"
 
-ECHO Settings have been restored.>> "%STARTDIR%\scripts\logs\debug.log"
-ECHO VPX close script end>> "%STARTDIR%\scripts\logs\debug.log"
+REM ECHO Restoring settings...>> "c:\Visual~1\Hiscores\scripts\logs\debug.log"
+REM CALL "c:\Visual~1\Hiscoresrestore_settings.bat"
+REM DEL "c:\Visual~1\Hiscoresrestore_settings.bat"
+
+REM ECHO Settings have been restored.>> "c:\Visual~1\Hiscores\scripts\logs\debug.log"
+REM ECHO VPX close script end>> "c:\Visual~1\Hiscores\scripts\logs\debug.log"
+
+cd "C:\Visual Pinball\vmvpin.github.io"
+git add .
+git commit -m "Hacked commit"
+git push
+
+ECHO VPX Close script complete for %GAMENAME% >> "c:\Visual~1\Hiscores\logs\debug.log"
+ECHO - >> "c:\Visual~1\Hiscores\logs\debug.log"
+
+
 exit /B
